1a62e2531df4023eaff8e87d18211605a0c4a6ce
itheima <itheima@itcast.cn>
2022-03-01 19:18:35 +0800

    完成了主页功能开发

---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function () {
+    var layer = layui.layer;
+
+    // 退出登录功能
+    $('#btn-tuichu').on('click', function () {
+        layer.confirm('确认退出登录吗？', {icon: 3, title:'提示'}, function(index){
+            //do something
+            // 清空本地token
+            localStorage.removeItem('token')
+            // 跳转页面到登录页面
+            location.href = '/login.html'
+            layer.close(index);
+          });
+    })
+
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        headers: { Authorization: localStorage.getItem('token') || "" },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            renderAvatar(res.data)
+        },
+        // 不论失败还是成功都会调用complete回调函数
+        // complete: function(res) {
+        //     // 在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+        //         // 清空本地token值
+        //         localStorage.removeItem('token')
+        //         // 跳转到登录页面
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+    // console.log(localStorage.getItem('token'))
+    // 渲染头像和名称
+    function renderAvatar(user) {
+        // console.log(user)
+        // 如果nickname不为空那么优先显示nickname后者显示username
+        var name = user.nickname || user.username
+        // 渲染名称
+        $('.text-name').html('欢迎&nbsp;&nbsp;' + name)
+        // 按需渲染用户的头像
+        if (user.user_pic === null) {
+            // 渲染昵称首字母文本头像
+            $('.text-avatar').html(name[0].toUpperCase()).show()
+            $('.layui-nav-img').hide()
+        } else {
+            // 渲染图片头像
+            $('.layui-nav-img').attr('src', user.user_pic).show
+            $('.text-avatar').hide()
+        }
+    }
+})
--- assets/lib/baseAPI.js
+++ assets/lib/baseAPI.js
@@ -2,4 +2,17 @@
 $.ajaxPrefilter(function(option) {
     option.url = "http://www.liulongbin.top:3007" + option.url
     // console.log(option)
+    // 统一为有权限的接口设置headers属性
+    if (option.url.indexOf('/my/') !== -1) {
+        option.herders = {Authorization:localStorage.getItem('token') || ''}
+    }
+    option.complete = function(res) {
+        // 在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 清空本地token值
+            localStorage.removeItem('token')
+            // 跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
